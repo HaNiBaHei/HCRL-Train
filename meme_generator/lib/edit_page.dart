23f34fe01baedf7a47e0f2dd709f4b75e6b163fb
HaNiBaHei <chinnapat2556@hotmail.com>
2021-10-24 15:05:06 +0700

    add export

--- meme_generator/lib/edit_page.dart
+++ meme_generator/lib/edit_page.dart
@@ -1,6 +1,14 @@
 // ignore_for_file: prefer_const_constructors, avoid_unnecessary_containers
 
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+
+import 'package:flutter/rendering.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:share/share.dart';
 
 class EditPage extends StatefulWidget {
   final String imageName;
@@ -13,6 +21,7 @@ class EditPage extends StatefulWidget {
 class _EditPageState extends State<EditPage> {
   String topText = "";
   String bottomText = "";
+  GlobalKey globalKey = new GlobalKey();
 
   void initState() {
     super.initState();
@@ -34,24 +43,29 @@ class _EditPageState extends State<EditPage> {
       ),
       body: ListView(
         children: [
-          Stack(
-            children: [
-              Image.asset('assets/meme/${widget.imageName}.jpg'),
-              Positioned(
-                top: 15,
-                left: 100,
-                child: buildStrokeText(topText),
-              ),
-              Positioned(
-                top: 350,
-                left: 55,
-                child: buildStrokeText(bottomText),
-              ),
-            ],
+          RepaintBoundary(
+            key: globalKey,
+            child: Stack(
+              children: [
+                Image.asset('assets/meme/${widget.imageName}.jpg'),
+                Positioned(
+                  top: 15,
+                  left: 100,
+                  child: buildStrokeText(topText),
+                ),
+                Positioned(
+                  top: 350,
+                  left: 55,
+                  child: buildStrokeText(bottomText),
+                )
+              ],
+            ),
           ),
           Container(
+            padding: EdgeInsets.symmetric(horizontal: 40),
             child: Column(
               children: [
+                SizedBox(height: 10),
                 TextField(
                   onChanged: (text) {
                     setState(() {
@@ -59,6 +73,8 @@ class _EditPageState extends State<EditPage> {
                     });
                   },
                   decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.grey[200],
                       hintText: "Top Text",
                       enabledBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.lime),
@@ -66,6 +82,7 @@ class _EditPageState extends State<EditPage> {
                       focusedBorder: OutlineInputBorder(
                           borderSide: BorderSide(color: Colors.lime))),
                 ),
+                SizedBox(height: 10),
                 TextField(
                   onChanged: (text) {
                     setState(() {
@@ -73,13 +90,27 @@ class _EditPageState extends State<EditPage> {
                     });
                   },
                   decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.grey[200],
                       hintText: "Bottom Text",
                       enabledBorder: OutlineInputBorder(
                         borderSide: BorderSide(color: Colors.lime),
                       ),
                       focusedBorder: OutlineInputBorder(
                           borderSide: BorderSide(color: Colors.lime))),
-                )
+                ),
+                Container(
+                    child: TextButton(
+                  child: Text(
+                    "Export",
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                  onPressed: () {
+                    exportMeme();
+                  },
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.lime)),
+                ))
               ],
             ),
           )
@@ -109,19 +140,23 @@ class _EditPageState extends State<EditPage> {
     );
   }
 
-  // void exportMeme() async {
-  //   RenderRepaintBoundary boundary =
-  //       globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
-  //   ui.Image image = await boundary.toImage();
+  void exportMeme() async {
+    try {
+      RenderRepaintBoundary boundary =
+          globalKey.currentContext!.findRenderObject() as RenderRepaintBoundary;
+      ui.Image image = await boundary.toImage();
 
-  //   final directory = (await getApplicationDocumentsDirectory()).path;
+      final directory = (await getApplicationDocumentsDirectory()).path;
 
-  //   ByteData byteData =
-  //       await image.toByteData(format: ui.ImageByteFormat.png) as ByteData;
-  //   Uint8List pngByte = byteData.buffer.asUint8List();
-  //   File imageFile = File('$directory/meme.png');
-  //   imageFile.writeAsBytesSync(pngByte);
+      ByteData byteData =
+          await image.toByteData(format: ui.ImageByteFormat.png) as ByteData;
+      Uint8List pngByte = byteData.buffer.asUint8List();
+      File imageFile = File('$directory/meme.png');
+      imageFile.writeAsBytesSync(pngByte);
 
-  //   Share.shareFiles(['$directory/meme.png']);
-  // }
+      Share.shareFiles(['$directory/meme.png']);
+    } catch (e) {
+      print(e);
+    }
+  }
 }
